8d64a6fde9fbd6a8995629ced4dae4a64c3881a1
Joseph Chen <joseph.chen@gmail.com>
2015-06-02 12:22:29 -0700

    Add environment variables support for puma.

--- puma/specs/default_spec.rb
+++ puma/specs/default_spec.rb
@@ -7,4 +7,20 @@ describe_recipe 'puma::default' do
   it 'installs puma' do
     assert system("#{node[:dependencies][:gem_binary]} list | grep puma | grep '#{node[:puma][:version]}'")
   end
+  it 'creates puma.conf for every application' do
+    node[:deploy].each do |application, deploy|
+      file("#{deploy[:deploy_to]}/shared/config/puma.conf").must_exist if node[:opsworks][:instance][:layers].include?("#{deploy[:application_type]}-app")
+    end
+  end
+
+  it 'contains correctly escaped environment variables in puma.conf files' do
+    node[:deploy].each do |application, deploy|
+      deploy[:environment_variables].each do |key, value|
+        if node[:opsworks][:instance][:layers].include?("#{deploy[:application_type]}-app")
+          file("#{deploy[:deploy_to]}/shared/config/puma.conf").must_include(key)
+          file("#{deploy[:deploy_to]}/shared/config/puma.conf").must_include(value.gsub("\"","\\\"")) unless value.blank?
+        end
+      end
+    end
+  end
 end
